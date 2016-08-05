d94144573439cdd0e96e8925eb34b0d7c9fc0cc1
huangzhi29 <huangzhi29@vip.qq.com>
2016-08-05 09:05:54 +0800

    first

---
+++ src/com/javferna/packtpub/book/mastering/synchronization/completable/LoadTask.java
@@ -0,0 +1,34 @@
+package com.javferna.packtpub.book.mastering.synchronization.completable;
+
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import com.javferna.packtpub.book.mastering.synchronization.data.Product;
+import com.javferna.packtpub.book.mastering.synchronization.data.ProductLoader;
+
+public class LoadTask implements Supplier<List<Product>> {
+
+	private Path path;
+	
+	public LoadTask (Path path) {
+		this.path=path;
+	}
+	@Override
+	public List<Product> get() {
+		List<Product> productList=null;
+		try {
+			productList = Files.walk(path, FileVisitOption.FOLLOW_LINKS).parallel()
+					.filter(f -> f.toString().endsWith(".txt")).map(ProductLoader::load).collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return productList;
+	}
+
+}
